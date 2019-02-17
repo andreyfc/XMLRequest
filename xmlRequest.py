2ba5c054f2e61c4cd3b109fdb40369bb00924c1f
andreyfc <30841856+andreyfc@users.noreply.github.com>
2019-02-16 17:53:33 -0800

    Update xmlRequest.py

--- xmlRequest.py
+++ xmlRequest.py
@@ -3,12 +3,11 @@ import xml.etree.ElementTree as ET
 
 xml_headers = { 'content-type': 'text/xml' }
 
-xml_request = open( 'request.xml' ).read()
+xml_request = open( 'file.xml' ).read()
 
-xml_response = requests.post( 'http://www.ahgora.com.br/ws/pontoweb.php', data=xml_request, headers=xml_headers )
+xml_response = requests.post( 'http://www.yourpage.com.br/ws/xxx.php', data=xml_request, headers=xml_headers )
 
 tree = ET.fromstring( xml_response.content )
 
 for elem in tree.iter():
-    # if elem.tag == 'status':
-        print ( elem.tag, ':', elem.text )
+    print ( elem.tag, ':', elem.text )
